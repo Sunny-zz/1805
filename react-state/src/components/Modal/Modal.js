27af200385439de38d128a5ae90304529e612bb8
Sunny-zz <b695340402@163.com>
2018-08-14 16:04:43 +0800

    react-state-modal

---
+++ react-state/src/components/Modal/Modal.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import './modal.css'
+class Modal extends Component {
+  state = {
+    show: false
+  }
+  open = () => {
+    this.setState({
+      show: true
+    })
+  }
+  close = () => {
+    this.setState({ show: false })
+  }
+  stop = event => {
+    // 阻止事件冒泡  event
+    event.stopPropagation()
+  }
+  render() {
+    const { show } = this.state
+    return (
+      <div className="modal">
+        <button className="open" onClick={this.open}>
+          Open
+        </button>
+        <div
+          className="wrap"
+          onClick={this.close}
+          style={{ display: show ? 'block' : 'none' }}
+        >
+          <div onClick={this.stop}>
+            <div className="header">
+              <h3>Basic Modal</h3>
+              <span onClick={this.close}>X</span>
+            </div>
+            <div className="main">
+              <p>some contents</p>
+              <p>some contents</p>
+              <p>some contents</p>
+            </div>
+            <div className="footer">
+              <button onClick={this.close}>取消</button>
+              <button onClick={this.close}>确定</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Modal
