ae100b240ea7692a2c6d08e8c10c2afe9bc8184e
Sunny-zz <b695340402@163.com>
2018-08-15 16:03:40 +0800

    react-lists update

--- react-lists/src/components/App/App.js
+++ react-lists/src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    const users = [
+  state = {
+    users: [
       {
         name: 'zzt',
         age: 20
@@ -13,25 +13,58 @@ class App extends Component {
       },
       {
         name: 'ym',
-        age: 22
+        age: 21
       },
       {
         name: 'wq',
-        age: 23
+        age: 20
       }
-    ]
+    ],
+    show: '全部'
+  }
+  showAll = () => {
+    this.setState({
+      show: '全部'
+    })
+  }
+  showSome = () => {
+    this.setState({
+      show: '大于21'
+    })
+  }
+  render() {
+    // 展示 大于 22 岁的所有人名
+    // const lists =
+    //   users.length === 0 ? (
+    //     <div>数据为空</div>
+    //   ) : (
+    //     <ul>
+    //       {users.filter(user => user.age >= 22).map(user => (
+    //         <li>{user.name}</li>
+    //       ))}
+    //     </ul>
+    //   )
+    // react 的标签数组直接写在 jsx 内，自动渲染
+    const { show, users } = this.state
+    const listsArr =
+      show === '全部' ? users : users.filter(user => user.age > 21)
     const lists =
-      users.length === 0 ? (
-        <div>数据为空</div>
+      listsArr.length === 0 ? (
+        <span>空</span>
       ) : (
         <ul>
-          {users.map(user => (
+          {listsArr.map(user => (
             <li>{user.name}</li>
           ))}
         </ul>
       )
-    // react 的标签数组直接写在 jsx 内，自动渲染
-    return <div>{lists}</div>
+    return (
+      <div>
+        <button onClick={this.showAll}>全部</button>
+        <button onClick={this.showSome}>大于21</button>
+        <div>{lists}</div>
+      </div>
+    )
   }
 }
 
