ff3244e0e6bbc7bf0e25701eb10096c649e9ed84
Sunny-zz <b695340402@163.com>
2018-08-17 10:47:45 +0800

    react-posts delete comment1

--- react-posts/src/components/App/App.js
+++ react-posts/src/components/App/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 class App extends Component {
   state = {
-    comments: []
+    comments: [],
+    comment: ''
   }
   componentDidMount = () => {
     const uri = 'http://localhost:3008/comments'
