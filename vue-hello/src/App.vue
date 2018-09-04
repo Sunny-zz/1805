77ef3230e625db50b4779b53bfee21600665e498
Sunny-zz <b695340402@163.com>
2018-09-04 14:59:19 +0800

    update

--- vue-hello/src/App.vue
+++ vue-hello/src/App.vue
@@ -4,7 +4,11 @@
     <div v-html="msg"></div>
     <span>{{ show ? '下午': '上午'}}</span>
     <span :class="{active:isActive}" :style="{fontSize:size}">测试</span>
-
+    <button v-show='isLogin' @click="handleClick">收藏</button>
+    <span v-if="!users.length">加载中...</span>
+    <ul v-else>
+      <li v-for="user in users" :key="user.id" @click="handleChange(user.id)" :class="{active: activeId === user.id}">{{user.name}}</li>
+    </ul>
   </div>
 </template>
 
@@ -17,12 +21,33 @@ export default {
   name: 'app',
   data: () => ({
     msg: '<h3>hello</h3>',
-    show: false,
+    show: true,
     isActive: true,
-    size: '100px'
+    size: '100px',
+    isLogin: true,
+    users: [
+      {
+        id: '1231',
+        name: 'zzt'
+      },
+      {
+        id: '12ss',
+        name: 'ym'
+      }
+    ],
+    activeId: ''
   }),
   components: {
     HelloWorld
+  },
+  methods: {
+    // 使用 this.$data 访问 vue 组件的 data 对象，直接对该对象下的属性进行修改就是更新了 data 数据。但是需要注意的是，修改 data 的函数必须是普通函数不能是箭头函数(箭头函数 this.$data 不会指向组件的 data)。
+    handleClick: function() {
+      this.$data.isLogin = false
+    },
+    handleChange: function(id) {
+      this.$data.activeId = id
+    }
   }
 }
 </script>
