1a1807c3145206bbb03a6a835ec26f1125df3744
Sunny-zz <b695340402@163.com>
2018-09-05 09:43:05 +0800

    addComment and delComment

--- vue-post/src/components/CommentBox.vue
+++ vue-post/src/components/CommentBox.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="box-wrap">
     <div>
-      <input type="text" v-model="comment">
-      <button>评论</button>
+      <input type="text" v-model.trim="comment">
+      <button @click="addComment">评论</button>
     </div>
     <div>
       <h4>评论</h4>
       <ul>
-        <li>哈哈哈</li>
-        <li>哈哈哈</li>
-        <li>哈哈哈</li>
+        <li v-for="comment in comments" :key="comment.id">{{comment.text}}
+          <button @click="delComment(comment.id)">删除</button>
+        </li>
       </ul>
     </div>
   </div>
@@ -18,8 +18,29 @@
 export default {
   name: 'commentBox',
   data: () => ({
-    comment: ''
-  })
+    comment: '',
+    comments: []
+  }),
+  methods: {
+    addComment() {
+      if (this.$data.comment) {
+        const newComment = {
+          text: this.$data.comment,
+          id: Math.floor(Math.random() * 10000)
+        }
+        this.$data.comments.push(newComment)
+        this.$data.comment = ''
+      } else {
+        alert('请输入有效字符')
+      }
+    },
+    delComment(id) {
+      this.$data.comments.splice(
+        this.$data.comments.findIndex(t => t.id === id),
+        1
+      )
+    }
+  }
 }
 </script>
 
