ab9dcdeec33f8c214d505352fadcb17acda74991
Sunny-zz <b695340402@163.com>
2018-09-12 10:14:58 +0800

    done

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -7,7 +7,8 @@
       </li>
     </ul>
     <span v-else>请添加待办事项</span>
-
+    <button @click="click">按钮</button>
+    <input type="text" ref="inp" v-focus="true">
   </div>
 </template>
 <script>
@@ -21,6 +22,16 @@ export default {
         .then(() => {
           this.$store.commit('completedTodo', id)
         })
+    },
+    click() {
+      console.log(this.$refs.inp.focus())
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus()
+      }
     }
   },
   computed: {
--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import './assets/gloable.css'
 import store from './store'
 Vue.config.productionTip = false
-
+// Vue.directive('focus', {
+//   // 当被绑定的元素插入到 DOM 中时……
+//   inserted: function(el) {
+//     // 聚焦元素
+//     el.focus()
+//   }
+// })
 new Vue({
   render: h => h(App),
   store
